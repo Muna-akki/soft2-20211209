d13a563208ad7006792c30b5298802cfcdeb19a1
Akihira Munakata <atagehaka@gmail.com>
2021-12-09 14:38:42 +0900

    change

--- struct_alignment.c
+++ struct_alignment.c
@@ -2,51 +2,55 @@
 
 typedef struct student
 {
-  int id;
-  char name[100];
-  int age;
-  double height;
-  double weight;
+    int id;
+    char name[100];
+    int age;
+    double height;
+    double weight;
 } Student;
 
 typedef struct tagged_student1
 {
-  int id;
-  char name[100];
-  int age;
-  double height;
-  double weight;
-  char tag;
+    int id;
+    char name[100];
+    int age;
+    double height;
+    double weight;
+    char tag;
 } TStudent1;
 
 typedef struct tagged_student2
 {
-  char tag;
-  int id;
-  char name[100];
-  int age;
-  double height;
-  double weight;
+    char tag;
+    int id;
+    char name[100];
+    int age;
+    double height;
+    double weight;
 } TStudent2;
 
 int main (int argc, char**argv)
 {
-  Student s_begin;
-  Student s1;
-  TStudent1 s2;
-  TStudent2 s3;
-  TStudent2 s_end;
+    Student s_begin;
+    Student s1;
+    TStudent1 s2;
+    TStudent2 s3;
+    TStudent2 s_end;
   
-  /* 以下に各構造体の先頭アドレスを表示するプログラムを書く */
-  /* Hint: printf で %p フォーマットを指定する*/
-  /* 逆順に表示（s_end, s3, s2, s1, s_begin の順）*/
+    /* 以下に各構造体の先頭アドレスを表示するプログラムを書く */
+    /* Hint: printf で %p フォーマットを指定する*/
+    /* 逆順に表示（s_end, s3, s2, s1, s_begin の順）*/
+    printf("%p %lu\n",&s_end, sizeof(Student));
+    printf("%p %lu\n",&s3, sizeof(Student));
+    printf("%p %lu\n",&s2, sizeof(TStudent1));
+    printf("%p %lu\n",&s1, sizeof(TStudent2));
+    printf("%p %lu\n",&s_begin, sizeof(TStudent2));
+    // do_something!!
 
-  // do_something!!
+    /* 以下には各構造体のサイズをsizeof演算子で計算し、表示する */
+    /* printf 表示には%zu を用いる*/
 
-  /* 以下には各構造体のサイズをsizeof演算子で計算し、表示する */
-  /* printf 表示には%zu を用いる*/
-
-  // do_something!!
+    // do_something!!
   
-  return 0;
+    return 0;
 }
